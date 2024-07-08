804600dcc343548544a8648535b3b8bf9650c013
Konrad Opach <opach16@outlook.com>
2024-07-08 18:45:38 +0200

    bug fixed

--- src/main/java/pl/kodilla/rps/GameRules.java
+++ src/main/java/pl/kodilla/rps/GameRules.java
@@ -2,11 +2,11 @@ package pl.kodilla.rps;
 
 public class GameRules {
     private final int[][] rules = new int[][]{
-            {0, 1, -1, 1, -1},
-            {-1, 0, 1, -1, 1},
-            {1, -1, 0, -1, 1},
-            {-1, 1, 1, 0, -1},
-            {1, -1, -1, 1, 0}
+            {1, 0, -1, 1, -1},
+            {0, -1, 1, -1, 1},
+            {-1, 1, 0, -1, 1},
+            {1, -1, 1, 0, -1},
+            {-1, 1, -1, 1, 0}
     };
 
     public GameRules() {
